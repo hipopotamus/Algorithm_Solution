43b7143306580c92f9fc5e32119bce1e4661c406
hipopotamus <qkqntjddn@gmail.com>
2022-07-07 17:47:34 +0900

    LeetCode 16번 문제_3Sum Closest

--- src/main/java/solution/Solution.java
+++ src/main/java/solution/Solution.java
@@ -1,4 +1,56 @@
 package solution;
 
+/*
+LeetCode 16번 문제
+https://leetcode.com/problems/3sum-closest/
+*/
+
+import java.util.Arrays;
+
 public class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int minDiff = 100000;
+
+        for (int i = 0; i < nums.length; i++) {   //숫자 하나 선택
+            int newTarget = target - nums[i];   //target에서 선택한 숫자만큼 뺀 값을 newTarget으로 정의
+
+            int start = 0;   //start, end point 설정, 선택한 숫자의 인덱스와 겹치면 변경
+            if (i == start) {
+                start++;
+            }
+            int end = nums.length - 1;
+            if (i == end) {
+                end--;
+            }
+
+            while (start != end) {   //newTarget에 근접하는 방향으로 start, end를 움직이면서 합을 구함
+                int sum = nums[start] + nums[end];
+                if (sum == newTarget) {   //숫자의 합이 target과 동일한 경우가 있다면 target을 바로 반환
+                    return target;
+                }
+
+                if (sum < newTarget) {
+                    if (start + 1 == i) {
+                        start += 2;
+                    } else {
+                        start++;
+                    }
+                }
+                if (sum >= newTarget) {
+                    if (end - 1 == i) {
+                        end -= 2;
+                    } else {
+                        end--;
+                    }
+                }
+
+                if (Math.abs(sum - newTarget) < Math.abs(minDiff)) {   //target과 가장 작은 차이가 나는 값을 갱신
+                    minDiff = sum - newTarget;
+                }
+            }
+        }
+
+        return target + minDiff;
+    }
 }
--- src/test/java/solution/SolutionTest.java
+++ src/test/java/solution/SolutionTest.java
@@ -1,6 +1,20 @@
 package solution;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 
 class SolutionTest {
+
+    @Test
+    void threeSumClosest() {
+        Solution solution = new Solution();
+        int [] testArray1 = {0, 0, 0};
+        int [] testArray2 = {-1,2,1,-4};
+        int result1 = solution.threeSumClosest(testArray1, 1);
+        int result2 = solution.threeSumClosest(testArray2, 1);
+
+        Assertions.assertThat(result1).isEqualTo(0);
+        Assertions.assertThat(result2).isEqualTo(2);
+    }
 }
