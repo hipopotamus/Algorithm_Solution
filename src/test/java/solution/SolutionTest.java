b90ff137364286578208798f4cb8bd02a10001b1
hipopotamus <qkqntjddn@gmail.com>
2022-07-16 16:21:31 +0900

    LeetCode 18번 문제_4Sum

--- src/test/java/solution/SolutionTest.java
+++ src/test/java/solution/SolutionTest.java
@@ -1,4 +1,21 @@
 package solution;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 class SolutionTest {
+
+    @Test
+    public void solutionTest() {
+        Solution solution = new Solution();
+
+        int[] nums = {0, 0, 0, 0, 0};
+        List<List<Integer>> lists = solution.fourSum(nums, 0);
+
+        for (List<Integer> list : lists) {
+            list.forEach(System.out::print);
+            System.out.println("");
+        }
+    }
 }
