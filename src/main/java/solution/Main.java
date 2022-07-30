dafae70b75cc35d91235c6793510dab851a159c5
hipopotamus <qkqntjddn@gmail.com>
2022-07-30 10:43:29 +0900

    백준 17425번 문제_약수의 합

--- src/main/java/solution/Main.java
+++ src/main/java/solution/Main.java
@@ -1,118 +1,41 @@
 package solution;
 
 /*
-백준 14500번 문제 테트로미노
-https://www.acmicpc.net/problem/14500
+백준 17425번 문제 약수의 합
+https://www.acmicpc.net/problem/17425
 */
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static class Location {   //row, col의 값을 갖는 클래스
-        int row;
-        int col;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        long[] sumDivisors = new long[1000000 + 1];
+        long[] cumulative = new long[1000000 + 1];
 
-        public Location(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-    }
-
-    public static class Tetromino {
-        Location[] blockList = new Location[3];
-        Location[] blockLocation = new Location[4];
-
-        public Tetromino(Location block1, Location block2, Location block3) {
-            this.blockList[0] = block1;
-            this.blockList[1] = block2;
-            this.blockList[2] = block3;
-        }
-
-        public void setBlockLocation(int row, int col) {   //주어진 좌표에서 연결된 block들을 세팅
-            this.blockLocation[0] = new Location(row, col);
-            for (int i = 1; i < 4; i++) {
-                this.blockLocation[i] = (new Location(row + blockList[i - 1].row, col + blockList[i - 1].col));
-            }
-        }
-
-        public void cycle() {   //테트로미노를 90도 회전시킨 좌표로 세팅
-            for (Location block : blockList) {
-                int tempRow = block.row;
-                block.row = block.col;
-                block.col = -tempRow;
-            }
-        }
-    }
-
-    public static int getMaxPoint(Tetromino tetromino, int[][] board) {   //주어진 테트로미노로 얻을 수 있는 최대 점수를 구하는 메서드
-        int result = 0;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                tetromino.setBlockLocation(i, j);   //시작 좌표 세팅
-
-                if (!isEdge(tetromino, board.length, board[0].length)) {   //블럭위치가 주어진 board의 경계값을 넘었다면 continue
-                    continue;
-                }
-
-                int point = 0;
-                for (Location location : tetromino.blockLocation) {   //테트로미노의 block 위치의 점수를 구함
-                    point += board[location.row][location.col];
-                }
-
-                if (point > result) {
-                    result = point;
+        //100만까지 반복문을 돌며 겹치지 않는 가능한 배수의 조합을 찾는다.
+        for (int i = 1; i <= 1000000; i++) {
+            for (int j = i; j <= (1000000 / i); j++) {
+                if (i == j) {
+                    sumDivisors[i * j] += i;   //배수의 조합이 존재한다면 해당 숫자에 배수들을 더해준다(배수 == 약수)
+                } else {
+                    sumDivisors[i * j] += i + j;
                 }
             }
         }
-        return result;
-    }
 
-    public static boolean isEdge(Tetromino tetromino, int rowEdge, int colEdge) {
-        for (Location location : tetromino.blockLocation) {
-            if (location.row < 0 || location.row >= rowEdge || location.col < 0 || location.col >= colEdge) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-
-    public static void main(String[] args) {
-        //입력 & 초기화 시작
-        Scanner scanner = new Scanner(System.in);
-        int row = scanner.nextInt();
-        int col = scanner.nextInt();
-
-        int result = 0;
-        int[][] board = new int[row][col];
-        ArrayList<Tetromino> tetrominoList = new ArrayList<>();
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                board[i][j] = scanner.nextInt();
-            }
+        //배수의 누적합을 계산
+        for (int i = 1; i <= 1000000; i++) {
+            cumulative[i] = cumulative[i - 1] + sumDivisors[i];
         }
 
-        tetrominoList.add(new Tetromino(new Location(0, 1), new Location(0, 2), new Location(0, 3))); //I
-        tetrominoList.add(new Tetromino(new Location(0, 1), new Location(1, 1), new Location(1, 0))); //O
-        tetrominoList.add(new Tetromino(new Location(0, 1), new Location(0, 2), new Location(-1, 1))); //T
-        tetrominoList.add(new Tetromino(new Location(0, 1), new Location(0, 2), new Location(-1, 2))); //L
-        tetrominoList.add(new Tetromino(new Location(0, 1), new Location(0, 2), new Location(-1, 0))); //J
-        tetrominoList.add(new Tetromino(new Location(0, 1), new Location(-1, 1), new Location(-1, 2))); //S
-        tetrominoList.add(new Tetromino(new Location(0, 1), new Location(1, 1), new Location(1, 2))); //Z
-        //입력 & 초기화 끝
-
-        for (Tetromino tetromino : tetrominoList) {   //각 테트로미노로 얻을 수 있는 최대 점수를 비교해서 result를 구한다.
-            for (int i = 0; i < 4; i++) {
-                int maxPoint = getMaxPoint(tetromino, board);
-                if (maxPoint > result) {
-                    result = maxPoint;
-                }
-                tetromino.cycle();
-            }
+        int n = Integer.parseInt(bf.readLine());
+        for (int i = 0; i < n; i++) {
+            int number = Integer.parseInt(bf.readLine());
+            bw.write(cumulative[number] + "\n");
         }
 
-        System.out.println(result);
+        bw.flush();   //버퍼에 출력할 내용을 한번에 담아서 출력(시간 단축)
     }
 }
