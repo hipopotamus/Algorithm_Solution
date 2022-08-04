0a4eee655c7f0fcc364203b515d627ae805f034f
hipopotamus <qkqntjddn@gmail.com>
2022-08-04 09:54:21 +0900

    백준 7562번 문제_나이트의 이동

--- src/main/java/solution/Main.java
+++ src/main/java/solution/Main.java
@@ -1,94 +1,84 @@
 package solution;
 
 /*
-백준 1707번 문제_이분그래프
-https://www.acmicpc.net/problem/1707
+백준 7562번 문제_나이트의 이동
+https://www.acmicpc.net/problem/7562
 */
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 
-    public static class Node {   //Node 정보를 담은 클래스
-        int number;
-        int team;   //이분그래프인지 판단하기 위한 flag. 1과 -1 두 팀으로 나뉜다.
+    public static class Knight {   //나이트의 정보를 담고있는 클래스
+        int row;
+        int col;
+        int dist = 0;   //주어진 초기 나이트로부터의 거리
         boolean check = false;
-        ArrayList<Node> edgeList = new ArrayList<>();
 
-        public Node(int number) {
-            this.number = number;
-        }
-
-        public void setTeam(int team) {
-            this.team = team;
+        public Knight(int row, int col) {
+            this.row = row;
+            this.col = col;
         }
     }
 
-    public static boolean bfs(Node[] nodes, int firstIndex) {   //bfs로 각 노드를 탐색하며, 연결된 노드가 같은 팀인지 확인한다.(연결된 노드가 같은 팀이면 이분그래프아님)
-        Queue<Node> queue = new LinkedList<>();
-        Node firstNode = nodes[firstIndex];
-        firstNode.setTeam(1);   //초기 팀은 1으로 설정
-        firstNode.check = true;
-        queue.offer(firstNode);
+    public static int bfs(Knight[][] knightArray, Knight startKnight, int destRow, int destCol) {   //초기 나이트로부터 이동할 수 있는 나이트의 위치를 탐색하며 거리를 계산한다.
+        if (startKnight.row == destRow && startKnight.col == destCol) {   //목표 좌표와 초기 나이트의 위치가 같으면 초기 나이트의 거리(=0)을 반환
+            return startKnight.dist;
+        }
+
+        int[] rowSet = {-2, -1, 1, 2, 2, 1, -1, -2};   //나이트가 이동할 수 있는 row와 col의 집합
+        int[] colSet = {1, 2, 2, 1, -1, -2, -2, -1};
+        Queue<Knight> queue = new LinkedList<>();
+        startKnight.check = true;
+        queue.offer(startKnight);
 
         while (!queue.isEmpty()) {
-            Node node = queue.poll();   //queue에서 node를 꺼낸다.
+            Knight knight = queue.poll();   //큐에서 나이트를 꺼낸 후, 꺼내진 나이트가 갈 수 있는 위치를 탐색한다.
 
-            for (int i = 0; i < node.edgeList.size(); i++) {   //node의 간선만큼 반복문을 돌린다.
-                Node nextNode = node.edgeList.get(i);
-                if (nextNode.check) {
-                    if (node.team == nextNode.team) {   //node에 연결된 노드(=nextNode)가 node와 같은 팀이라면 이분그래프 아님
-                        return false;
-                    }
-                } else {
-                    nextNode.setTeam(node.team * -1);   //nextNode를 한번도 탐색하지 않았다면 node와 다른 team을 설정
-                    nextNode.check = true;
-                    queue.offer(nextNode);
+            for (int i = 0; i < 8; i++) {   //나이트가 이동할 수 있는 8방향을 반복문으로 돌리며 탐색
+                int nextRow = knight.row + rowSet[i];
+                int nextCol = knight.col + colSet[i];
+                if (nextRow < 0 || nextRow >= knightArray.length || nextCol < 0 || nextCol >= knightArray.length
+                        || knightArray[nextRow][nextCol].check) {   //나이트가 이동할 좌표가 체스판의 범위를 넘어가거나 이미 탐색한 좌표라면 반복문을 넘긴다.
+                    continue;
                 }
+
+                Knight nextKnight = knightArray[nextRow][nextCol];
+                nextKnight.dist = knight.dist + 1;
+                nextKnight.check = true;
+
+                if (nextKnight.row == destRow && nextKnight.col == destCol) {   //다음 나이트(=nextKnight)의 위치가 목표 좌표라면 다음 나이트의 거리를 반환한다.
+                    return nextKnight.dist;
+                }
+                queue.offer(nextKnight);
             }
         }
-        return true;
+        return -1;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int total = scanner.nextInt();
 
-        for (int i = 0; i < total; i++) {   //주어진 시행회수만큼 반복문을 돌린다.
+        while (total-- > 0) {
             //초기화 시작
-            boolean result = true;
-            int nodeSize = scanner.nextInt();
-            int edgeSize = scanner.nextInt();
-            Node[] nodes = new Node[nodeSize + 1];
-            for (int j = 0; j <= nodeSize; j++) {
-                nodes[j] = new Node(i);
-            }
+            int size = scanner.nextInt();
 
-            for (int k = 0; k < edgeSize; k++) {   //인접리스트 생성
-                Node toNode = nodes[scanner.nextInt()];
-                Node fromNode = nodes[scanner.nextInt()];
-                toNode.edgeList.add(fromNode);
-                fromNode.edgeList.add(toNode);
+            Knight[][] knightArray = new Knight[size][size];
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    knightArray[i][j] = new Knight(i, j);
+                }
             }
+            Knight startKnight = knightArray[scanner.nextInt()][scanner.nextInt()];
+            int destRow = scanner.nextInt();
+            int destCol = scanner.nextInt();
             //초기화 끝
 
-            for (int l = 1; l <= nodeSize; l++) {   //각 노드에 bfs를 적용해서 모든 노드를 탐색하고 이분그래프 여부를 판단한다.
-                if (nodes[l].check) {
-                    continue;
-                }
-                if (!bfs(nodes, l)) {   //bfs에 의해 이분그래프가 아니라고 판단되면 result를 false로 변경
-                    result = false;
-                }
-            }
-
-            if (result) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
-            }
+            int result = bfs(knightArray, startKnight, destRow, destCol);
+            System.out.println(result);
         }
     }
 }
