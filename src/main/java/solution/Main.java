151055e6086f8bd60d233e3e87657b41aafa0fe9
hipopotamus <qkqntjddn@gmail.com>
2022-08-11 09:39:46 +0900

    백준 5373번 문제_큐빙

--- src/main/java/solution/Main.java
+++ src/main/java/solution/Main.java
@@ -1,131 +1,203 @@
 package solution;
 
 /*
-백준 15662번 문제_톱니바퀴(2)
-https://www.acmicpc.net/problem/15662
+백준 5373번 문제_큐빙
+https://www.acmicpc.net/problem/5373
 */
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static class Gear {
-        //톱니바퀴의 극을 담은 list
-        ArrayList<Integer> poleList = new ArrayList<>();
 
-        //톱니바퀴가 인접하는 방향(왼쪽에서 인접하는지 오른쪽에서 인접하는지), 회전 방법, 인접한 극을 받아서 Gear를 회전하는 메서드
-        //결과적으로 이 Gear가 회전한 방법을 반환한다.(회전하지 않으면 0반환)
-        public int linkCycle(boolean isLeftStream, int cycleDirection, int pole) {
-            int linkPole;
-            //톱니바퀴가 왼쪽으로부터 인접한다면 이 Gear에서 인접하는 극은 오른쪽이 된다(12로부터 3번째).
-            if (isLeftStream) {
-                linkPole = poleList.get(2);
-                //오른쪽은 12시로부터 6번째
-            } else {
-                linkPole = poleList.get(6);
+    //면을 나타내는 클래스
+    public static class Plane {
+        //면의 색 정보
+        char[][] colors = new char[3][3];
+
+        //면을 90도로 회전시키는 메서드
+        public void cycleOnePlane(char direction) {
+            char[][] copyColors = copyOfArray(colors);
+            if (direction == '+') {
+                for (int i = 0; i < colors.length; i++) {
+                    for (int j = 0; j < colors[0].length; j++) {
+                        colors[j][colors[0].length - 1 - i] = copyColors[i][j];
+                    }
+                }
+            } else if (direction == '-') {
+                for (int i = 0; i < colors.length; i++) {
+                    for (int j = 0; j < colors[0].length; j++) {
+                        colors[colors[0].length - 1 - j][i] = copyColors[i][j];
+                    }
+                }
             }
-
-            //맞다은 두 극이 같으면 Gear를 회전하지 않는다.
-            if (linkPole == pole) {
-                return 0;
-            }
-
-            //맞다은 두극이 다르면 Gear를 인접한 기어가 회전한 방법의 반대 방법으로 회전시킨다.
-            cycle(-1 * cycleDirection);
-            return -1 * cycleDirection;
         }
 
-        //회전 방법을 받고 Gear를 회전하는 메서드
-        private void cycle(int cycleDirection) {
-            //시계 방향
-            if (cycleDirection == 1) {
-                Integer endPole = poleList.get(poleList.size() - 1);
-                poleList.remove(poleList.size() - 1);
-                poleList.add(0, endPole);
-
+        private char[][] copyOfArray(char[][] chars) {
+            char[][] copyChar = new char[chars.length][chars[0].length];
+            for (int i = 0; i < chars.length; i++) {
+                copyChar[i] = Arrays.copyOf(chars[i], chars[i].length);
             }
-            //반시계 방향
-            if (cycleDirection == -1) {
-                Integer firstPole = poleList.get(0);
-                poleList.remove(0);
-                poleList.add(firstPole);
-            }
+            return copyChar;
         }
     }
 
-    //index 위치의 Gear를 회전시키고 인접한 Gear들을 연쇠적으로 회전시키는 메서드
-    public static void cycleSimulation(Gear[] gearArray, int index, int cycleDirection) {
-        //초기 Gear
-        Gear gear = gearArray[index];
-        //초기 Gear로 부터 오른쪽 방향을 탐색할 때, Gear(회전 될쪽)와 인접한 Gear(회전 한쪽)간에 인접하는 오른쪽 극
-        Integer tempRightLinkPole = gear.poleList.get(2);
-        //초기 Gear로 부터 왼쪽 방향을 탐색할 때,Gear(회전 될쪽)와 인접한 Gear(회전 한쪽)간에 인접하는 왼쪽 극
-        Integer tempLeftLinkPole = gear.poleList.get(6);
+    //큐브를 나타내는 클래스
+    public static class Cube {
+        //각 면을 나타내는 배열
+        Plane[] planes = new Plane[6];
 
-        int tempCycleDirection = cycleDirection;
-        //초기 회전한 Gear로 부터 왼쪽에 있는 Gear들을 연속적으로 회전시킨다.
-        for (int i = index - 1; i >= 0; i--) {
-            //왼쪽에 있는 Gear
-            Gear leftGear = gearArray[i];
-            //회전 시키기 전의 인접하는 왼쪽 극을 저장한다.
-            Integer leftLinkPole = leftGear.poleList.get(6);
-            //회전
-            tempCycleDirection = leftGear.linkCycle(true, tempCycleDirection, tempLeftLinkPole);
-            //만약 회전하지 않는다면 반복문을 끝낸다.
-            if (tempCycleDirection == 0) {
-                break;
+        //주어진 명령에 따라 큐브를 돌리는 메서드
+        public void cycleCube(String orderSet) {
+            char order = orderSet.charAt(0);
+            char direction = orderSet.charAt(1);
+            char[] temp = new char[3];
+
+            if (order == 'F') {
+                planes[3].cycleOnePlane(direction);
+                if (direction == '-') {
+                    for (int i = 0; i < 3; i++) temp[i] = planes[0].colors[0][i];
+                    for (int i = 0; i < 3; i++) planes[0].colors[0][i] = planes[4].colors[i][2];
+                    for (int i = 0; i < 3; i++) planes[4].colors[2 - i][2] = planes[5].colors[2][i];
+                    for (int i = 0; i < 3; i++) planes[5].colors[2][i] = planes[2].colors[i][0];
+                    for (int i = 0; i < 3; i++) planes[2].colors[2 - i][0] = temp[i];
+                    //반대로 돌리는것은 반대 방향에서 행해지는 작업을 거꾸로 하면된다.
+                } else {
+                    for (int i = 0; i < 3; i++) temp[i] = planes[0].colors[0][i];
+                    for (int i = 0; i < 3; i++) planes[0].colors[0][2 - i] = planes[2].colors[i][0];
+                    for (int i = 0; i < 3; i++) planes[2].colors[i][0] = planes[5].colors[2][i];
+                    for (int i = 0; i < 3; i++) planes[5].colors[2][2 - i] = planes[4].colors[i][2];
+                    for (int i = 0; i < 3; i++) planes[4].colors[i][2] = temp[i];
+                }
+                return;
             }
-            //다음 왼쪽에 있는 Gear를 회전할 지 판단하기 위해서 현재 회전시킨 Gear의 회전시키기전 인접한 극을 다음 반복문으로 넘긴다.
-            tempLeftLinkPole = leftLinkPole;
+            //B는 F와 돌려지는 면이 같다. B는 F의 반대면이기 때문에 돌려지는 면의 원소의 위치가 반대 편이다.
+            // B는 F의 반대면이기 때문에 F에서 '-' 방향에 돌려지는 면이 B에서 '+'일 때 돌려지는 면과 같다.(반대 방향도 동일 원리 적용)
+            //이와 같은 성질은 LR, UD에도 적용된다.
+            if (order == 'B') {
+                planes[1].cycleOnePlane(direction);
+                if (direction == '+') {
+                    for (int i = 0; i < 3; i++) temp[i] = planes[0].colors[2][i];
+                    for (int i = 0; i < 3; i++) planes[0].colors[2][i] = planes[4].colors[i][0];
+                    for (int i = 0; i < 3; i++) planes[4].colors[2 - i][0] = planes[5].colors[0][i];
+                    for (int i = 0; i < 3; i++) planes[5].colors[0][i] = planes[2].colors[i][2];
+                    for (int i = 0; i < 3; i++) planes[2].colors[2 - i][2] = temp[i];
+                } else {
+                    for (int i = 0; i < 3; i++) temp[i] = planes[0].colors[2][i];
+                    for (int i = 0; i < 3; i++) planes[0].colors[2][2 - i] = planes[2].colors[i][2];
+                    for (int i = 0; i < 3; i++) planes[2].colors[i][2] = planes[5].colors[0][i];
+                    for (int i = 0; i < 3; i++) planes[5].colors[0][2 - i] = planes[4].colors[i][0];
+                    for (int i = 0; i < 3; i++) planes[4].colors[i][0] = temp[i];
+                }
+                return;
+            }
+            if (order == 'L') {
+                planes[4].cycleOnePlane(direction);
+                if (direction == '-') {
+                    for (int i = 0; i < 3; i++) temp[i] = planes[0].colors[i][0];
+                    for (int i = 0; i < 3; i++) planes[0].colors[2 - i][0] = planes[1].colors[i][2];
+                    for (int i = 0; i < 3; i++) planes[1].colors[2 - i][2] = planes[5].colors[i][0];
+                    for (int i = 0; i < 3; i++) planes[5].colors[i][0] = planes[3].colors[i][0];
+                    for (int i = 0; i < 3; i++) planes[3].colors[i][0] = temp[i];
+                } else {
+                    for (int i = 0; i < 3; i++) temp[i] = planes[0].colors[i][0];
+                    for (int i = 0; i < 3; i++) planes[0].colors[i][0] = planes[3].colors[i][0];
+                    for (int i = 0; i < 3; i++) planes[3].colors[i][0] = planes[5].colors[i][0];
+                    for (int i = 0; i < 3; i++) planes[5].colors[2 - i][0] = planes[1].colors[i][2];
+                    for (int i = 0; i < 3; i++) planes[1].colors[2 - i][2] = temp[i];
+                }
+            }
+            if (order == 'R') {
+                planes[2].cycleOnePlane(direction);
+                if (direction == '+') {
+                    for (int i = 0; i < 3; i++) temp[i] = planes[0].colors[i][2];
+                    for (int i = 0; i < 3; i++) planes[0].colors[2 - i][2] = planes[1].colors[i][0];
+                    for (int i = 0; i < 3; i++) planes[1].colors[2 - i][0] = planes[5].colors[i][2];
+                    for (int i = 0; i < 3; i++) planes[5].colors[i][2] = planes[3].colors[i][2];
+                    for (int i = 0; i < 3; i++) planes[3].colors[i][2] = temp[i];
+                } else {
+                    for (int i = 0; i < 3; i++) temp[i] = planes[0].colors[i][2];
+                    for (int i = 0; i < 3; i++) planes[0].colors[i][2] = planes[3].colors[i][2];
+                    for (int i = 0; i < 3; i++) planes[3].colors[i][2] = planes[5].colors[i][2];
+                    for (int i = 0; i < 3; i++) planes[5].colors[2 - i][2] = planes[1].colors[i][0];
+                    for (int i = 0; i < 3; i++) planes[1].colors[2 - i][0] = temp[i];
+                }
+            }
+            if (order == 'U') {
+                planes[5].cycleOnePlane(direction);
+                if (direction == '+') {
+                    for (int i = 0; i < 3; i++) temp[i] = planes[1].colors[0][i];
+                    for (int i = 0; i < 3; i++) planes[1].colors[0][i] = planes[4].colors[0][i];
+                    for (int i = 0; i < 3; i++) planes[4].colors[0][i] = planes[3].colors[0][i];
+                    for (int i = 0; i < 3; i++) planes[3].colors[0][i] = planes[2].colors[0][i];
+                    for (int i = 0; i < 3; i++) planes[2].colors[0][i] = temp[i];
+                } else {
+                    for (int i = 0; i < 3; i++) temp[i] = planes[1].colors[0][i];
+                    for (int i = 0; i < 3; i++) planes[1].colors[0][i] = planes[2].colors[0][i];
+                    for (int i = 0; i < 3; i++) planes[2].colors[0][i] = planes[3].colors[0][i];
+                    for (int i = 0; i < 3; i++) planes[3].colors[0][i] = planes[4].colors[0][i];
+                    for (int i = 0; i < 3; i++) planes[4].colors[0][i] = temp[i];
+                }
+            }
+            if (order == 'D') {
+                planes[0].cycleOnePlane(direction);
+                if (direction == '-') {
+                    for (int i = 0; i < 3; i++) temp[i] = planes[1].colors[2][i];
+                    for (int i = 0; i < 3; i++) planes[1].colors[2][i] = planes[4].colors[2][i];
+                    for (int i = 0; i < 3; i++) planes[4].colors[2][i] = planes[3].colors[2][i];
+                    for (int i = 0; i < 3; i++) planes[3].colors[2][i] = planes[2].colors[2][i];
+                    for (int i = 0; i < 3; i++) planes[2].colors[2][i] = temp[i];
+                } else {
+                    for (int i = 0; i < 3; i++) temp[i] = planes[1].colors[2][i];
+                    for (int i = 0; i < 3; i++) planes[1].colors[2][i] = planes[2].colors[2][i];
+                    for (int i = 0; i < 3; i++) planes[2].colors[2][i] = planes[3].colors[2][i];
+                    for (int i = 0; i < 3; i++) planes[3].colors[2][i] = planes[4].colors[2][i];
+                    for (int i = 0; i < 3; i++) planes[4].colors[2][i] = temp[i];
+                }
+            }
+
         }
 
-        tempCycleDirection = cycleDirection;
-        for (int i = index + 1; i < gearArray.length; i++) {
-            Gear rightGear = gearArray[i];
-            Integer rightLinkPole = rightGear.poleList.get(2);
-            tempCycleDirection = rightGear.linkCycle(false, tempCycleDirection, tempRightLinkPole);
-            if (tempCycleDirection == 0) {
-                break;
-            }
-            tempRightLinkPole = rightLinkPole;
-        }
-
-        gear.cycle(cycleDirection);
     }
 
     public static void main(String[] args) {
-        //초기화 시작
-        int result = 0;
         Scanner scanner = new Scanner(System.in);
+        char[] charset = {'y', 'o', 'b', 'r', 'g', 'w'};
 
-        int gearSize = scanner.nextInt();
-        //주어진 Gear를 담는 배열
-        Gear[] gearArray = new Gear[gearSize];
-
-        for (int i = 0; i < gearArray.length; i++) {
-            String next = scanner.next();
-            Gear gear = new Gear();
-            for (int j = 0; j < next.length(); j++) {
-                gear.poleList.add(Integer.parseInt(String.valueOf(next.charAt(j))));
+        int total = scanner.nextInt();
+        for (int l = 0; l < total; l++) {
+            //cube 초기화 시작
+            Cube cube = new Cube();
+            for (int k = 0; k < charset.length; k++) {
+                Plane plane = new Plane();
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        plane.colors[i][j] = charset[k];
+                    }
+                }
+                cube.planes[k] = plane;
             }
-            gearArray[i] = gear;
-        }
-        //초기화 끝
+            //cube 초기화 끝
 
-        int caseSize = scanner.nextInt();
-        //주어지 케이스 횟수만큼 Gear를 회전시킨다.
-        for (int i = 0; i < caseSize; i++) {
-            int index = scanner.nextInt() - 1;
-            int cycleDirection = scanner.nextInt();
-            cycleSimulation(gearArray, index, cycleDirection);
-        }
+            //cube 돌리기
+            int length = scanner.nextInt();
+            for (int j = 0; j < length; j++) {
+                String next = scanner.next();
+                cube.cycleCube(next);
+            }
 
-        //12시 방향이 S극인 Gear의 개수를 구한다.
-        for (int i = 0; i < gearSize; i++) {
-            if (gearArray[i].poleList.get(0) == 1) {
-                result++;
+            //윗면 출력
+            for (int i = 0; i < cube.planes[5].colors.length; i++) {
+                for (int j = 0; j < cube.planes[5].colors[0].length; j++) {
+                    System.out.print(cube.planes[5].colors[i][j]);
+                }
+                if (i != cube.planes[5].colors.length - 1) {
+                    System.out.println();
+                }
+            }
+            if (l != total - 1) {
+                System.out.println();
             }
         }
-        System.out.println(result);
     }
 }
