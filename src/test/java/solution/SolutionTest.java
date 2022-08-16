a9d60bb085877fb4bc7d4e1d466be1256d793415
hipopotamus <qkqntjddn@gmail.com>
2022-08-17 08:48:44 +0900

    백준 3020번 문제_개똥벌레

--- src/test/java/solution/SolutionTest.java
+++ src/test/java/solution/SolutionTest.java
@@ -6,8 +6,5 @@ class SolutionTest {
 
     @Test
     public void solutionTest() {
-        Solution solution = new Solution();
-        int[] output = solution.quickSort(new int[]{1, 2, 43, 100, 21});
-        System.out.println(output);
     }
 }
