2aa9aa4ff9ad932e78e85084aca3ff48da6099b8
hipopotamus <qkqntjddn@gmail.com>
2022-08-23 14:22:59 +0900

    백준 2468번 문제_안전영역

--- src/main/java/solution/Main.java
+++ src/main/java/solution/Main.java
@@ -1,106 +1,135 @@
 package solution;
 
 /*
-백준 1753번 문제_최단경로 구하기
-https://www.acmicpc.net/problem/1753
+백준 2468번 문제_안전영역
+https://www.acmicpc.net/problem/2468
 */
 
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    //간선을 나타내는 클래스
-    private static class Edge {
-        //간선이 향하는 노드
-        int to;
-        //해당 간선의 가중치
-        int payOff;
+    static int[] dRow = {-1, 0, 1, 0};
+    static int[] dCol = {0, 1, 0, -1};
 
-        public Edge(int to, int payOff) {
-            this.to = to;
-            this.payOff = payOff;
-        }
-    }
-
-    //노드(정점)을 나타내는 클래스
+    //노드(지역)을 나타내는 클래스
     private static class Node {
-        int number;
-        //시작점 부터 해당 노드까지 이동하는데 필요한 비용
-        int totalPayOff = 10000000;
+        int row;
+        int col;
+        int height;
         boolean check = false;
-        ArrayList<Edge> edgeList = new ArrayList<>();
+        //침수 여부를 나타내는 flag
+        boolean flooding = false;
 
-        public Node(int number) {
-            this.number = number;
+        public Node(int row, int col, int height) {
+            this.row = row;
+            this.col = col;
+            this.height = height;
         }
     }
 
-    //다익스트라를 사용해서 start로 부터 각 노드까지의 최소 비용을 구한다.
-    private static void bfs(Node[] nodeArr, int start) {
-        /*
-        Queue에는 Edge가 들어간다.
-        이 Queue에 들어가는 Edge는 Node의 Edge와는 다르다.
-        payOff가 to노드로 이어지는 간선의 가중치가 아니라 start로 부터 to까지의 이동하는데 필요한 비용을 의미한다.
-        해당 Queue는 start로 부터 to까지 이동하는데 필요한 비용을 기준으로 오름차순 정렬된다.
-        */
-        PriorityQueue<Edge> queue = new PriorityQueue<>((e1, e2) -> e1.payOff < e2.payOff ? -1 : 1);
-        nodeArr[start].totalPayOff = 0;
-        queue.offer(new Edge(start, 0));
+    //주어진 위치로부터 연결된 영역을 탐색
+    private static void bfs(Node[][] nodeArr, int row, int col) {
+        Queue<Node> queue = new LinkedList<>();
+        Node startNode = nodeArr[row][col];
+        startNode.check = true;
+        queue.offer(startNode);
 
         while (!queue.isEmpty()) {
-            Edge queueEdge = queue.poll();
-            Node node = nodeArr[queueEdge.to];
+            Node node = queue.poll();
 
-            //아래 반복문에서는 간선을 Queue에 넣는것이기 때문에 node에 대한 check를 여기서 한다.
-            if (node.check) {
-                continue;
-            }
-            node.check = true;
-            //Queue에서 뽑은 Edge의 to는 start에서 to로 가는 총 비용이 가장 적다.
-            //to와 연결된 간선을 전부 탐색하며 이어진 노드의 totalPayOff를 갱신한다.
-            //탐색한 간선은 전부 Queue에 넣어준다. 간선의 노드가 check됐다면 나중에 반복문에서 알아서 걸러진다.
-            for (int i = 0; i < node.edgeList.size(); i++){
-                Edge edge = node.edgeList.get(i);
-                Node nextNode = nodeArr[edge.to];
-                int totalPayOff = node.totalPayOff + edge.payOff;
-                //간선과 연결된 노드의 totalPayOff를 갱신한다.
-                if (nextNode.totalPayOff <= totalPayOff) {
+            for (int i = 0; i < 4; i++) {
+                int nextRow = node.row + dRow[i];
+                int nextCol = node.col + dCol[i];
+                if (nextRow < 0 || nextRow >= nodeArr.length || nextCol < 0 || nextCol >= nodeArr.length
+                        || nodeArr[nextRow][nextCol].check || nodeArr[nextRow][nextCol].flooding) {
                     continue;
                 }
-                nextNode.totalPayOff = totalPayOff;
-                queue.offer(new Edge(nextNode.number, nextNode.totalPayOff));
+                Node nextNode = nodeArr[nextRow][nextCol];
+                nextNode.check = true;
+                queue.offer(nextNode);
+            }
+        }
+    }
+
+    //주어진 nodeArr에서 안전한 영역의 개수를 찾는 메서드
+    private static int getCount(Node[][] nodeArr) {
+        int count = 0;
+        for (int i = 0; i < nodeArr.length; i++) {
+            for (int j = 0; j < nodeArr.length; j++) {
+                if (nodeArr[i][j].check || nodeArr[i][j].flooding) {
+                    continue;
+                }
+                bfs(nodeArr, i, j);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    //강수량을 0부터 100까지 높히면서 각 경우에 안전한 영역의 개수를 탐색하고 그 최대값을 구하는 메서드
+    private static int getMaxCount(Node[][] nodeArr) {
+
+        int maxCount = 0;
+
+        for (int rainHeight = 0; rainHeight <= 100; rainHeight++) {
+            //nodeArr의 모든 check를 false로 초기화한다.
+            initNode(nodeArr);
+            //주어진 강수량 만큼 nodeArr의 node를 침수시킨다.
+            flood(nodeArr, rainHeight);
+            int count = getCount(nodeArr);
+            //전 지역이 침수되었다면 반복문을 미리 종료한다.
+            if (count == 0) {
+                break;
+            }
+            if (count > maxCount) {
+                maxCount = count;
+            }
+        }
+        return maxCount;
+    }
+
+    //주어진 nodeArr에서 node의 check를 전부 false로 초기화하는 메서드
+    private static void initNode(Node[][] nodeArr) {
+        for (int i = 0; i < nodeArr.length; i++) {
+            for (int j = 0; j < nodeArr.length; j++) {
+                if (nodeArr[i][j].flooding) {
+                    continue;
+                }
+                nodeArr[i][j].check = false;
+            }
+        }
+    }
+
+    //주어진 강수량에 따라 nodeArr의 node를 침수시키는 메서드
+    private static void flood(Node[][] nodeArr, int rainHeight) {
+        for (int i = 0; i < nodeArr.length; i++) {
+            for (int j = 0; j < nodeArr.length; j++) {
+                //강수량은 증가하도록 주어지기 때문에 이미 침수된 지역은 넘어간다.
+                if (nodeArr[i][j].flooding) {
+                    continue;
+                }
+                if (nodeArr[i][j].height <= rainHeight) {
+                    nodeArr[i][j].flooding = true;
+                }
             }
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int nodeSize = scanner.nextInt();
-        int edgeSize = scanner.nextInt();
-        int start = scanner.nextInt();
-        Node[] nodeArr = new Node[nodeSize + 1];
+        int size = scanner.nextInt();
+        Node[][] nodeArr = new Node[size][size];
 
-        for (int i = 1; i <= nodeSize; i++) {
-            nodeArr[i] = new Node(i);
-        }
-        for (int i = 0; i < edgeSize; i++) {
-            int from = scanner.nextInt();
-            int to = scanner.nextInt();
-            int payOff = scanner.nextInt();
-
-            nodeArr[from].edgeList.add(new Edge(to, payOff));
-        }
-
-        bfs(nodeArr, start);
-
-        for (int i = 1; i < nodeArr.length; i++) {
-            if (nodeArr[i].totalPayOff == 10000000) {
-                System.out.println("INF");
-            } else {
-                System.out.println(nodeArr[i].totalPayOff);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int height = scanner.nextInt();
+                nodeArr[i][j] = new Node(i, j, height);
             }
         }
+
+        int count = getMaxCount(nodeArr);
+        System.out.println(count);
     }
 }
