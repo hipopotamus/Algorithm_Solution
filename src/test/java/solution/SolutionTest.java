8189222245f9dbb2ee56f30c7df128c3fef11b5e
hipopotamus <qkqntjddn@gmail.com>
2022-12-29 16:34:30 +0900

    백준 13913번 문제_숨바꼭질4

--- src/test/java/solution/SolutionTest.java
+++ src/test/java/solution/SolutionTest.java
@@ -4,54 +4,6 @@ import org.junit.jupiter.api.Test;
 
 class SolutionTest {
 
-    static String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-
-    public int solution(String s) {
-
-        int answer = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-
-            char point = s.charAt(i);
-            int number = 0;
-
-            if (point < '0' || point > '9') {
-                number = convert(s, i);
-                i += numbers[number].length() - 1;
-            } else {
-                number = Integer.parseInt(String.valueOf(point));
-            }
-
-            if (answer == 0) {
-                answer += number;
-            } else {
-                answer = answer * 10 + number;
-            }
-        }
-
-        return answer;
-    }
-
-    public int convert(String s, int index) {
-
-        for (int i = 0; i < numbers.length; i++) {
-
-            String number = numbers[i];
-            int length = number.length();
-            if (index + length > s.length()) {
-                continue;
-            }
-
-            String subNumber = s.substring(index, index + length);
-
-            if (number.equals(subNumber)) {
-                return i;
-            }
-        }
-
-        return -1;
-    }
-
     @Test
     public void solutionTest() {
     }
