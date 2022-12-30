4abe97deac32970f9dabf903f78be5cd5e7d9ef0
hipopotamus <qkqntjddn@gmail.com>
2022-12-30 17:38:06 +0900

    백준 13649번 문제_숨바꼭질3

--- src/main/java/solution/Main.java
+++ src/main/java/solution/Main.java
@@ -3,80 +3,58 @@ package solution;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] moveSet = {-3, 1, 3, -1};
-    
+    static int[] moveSet = {-1, 1, 2};
+
     private static class Node {
 
-        int emptyLocation;
-        int[] numberArr = new int[9];
-        int count = 0;
+        int number;
+        int time;
 
-        public Node(int beforeEmptyLocation, int emptyLocation, int[] numberArr, int count) {
-            this.count = count;
-            this.emptyLocation = emptyLocation;
-
-            for (int i = 0; i < numberArr.length; i++) {
-                this.numberArr[i] = numberArr[i];
-            }
-
-            int temp = this.numberArr[emptyLocation];
-            this.numberArr[emptyLocation] = this.numberArr[beforeEmptyLocation];
-            this.numberArr[beforeEmptyLocation] = temp;
+        public Node(int number, int time) {
+            this.number = number;
+            this.time = time;
         }
-
-        public Node() {}
     }
 
-    private static int intArrToInt(int[] numberArr) {
+    private static Node bfs(Node firstNode, int dist, boolean[] check) {
 
-        StringBuilder stringBuilder = new StringBuilder();
+        Deque<Node> deque = new LinkedList<>();
+        deque.offer(firstNode);
+        check[firstNode.number] = true;
 
-        for (int i : numberArr) {
-            stringBuilder.append(i);
-        }
+        while (!deque.isEmpty()) {
 
-        return Integer.parseInt(stringBuilder.toString());
-    }
+            Node currentNode = deque.poll();
+            check[currentNode.number] = true;
 
-    private static Node bfs(Node firstNode, Map<Integer, Boolean> check, int dist) {
-
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(firstNode);
-
-        int puzzleSet = intArrToInt(firstNode.numberArr);
-        check.put(puzzleSet, true);
-
-        while (!queue.isEmpty()) {
-
-            Node currentNode = queue.poll();
-
-            if (intArrToInt(currentNode.numberArr) == dist) {
+            if (currentNode.number == dist) {
                 return currentNode;
             }
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 3; i++) {
 
-                int nextEmptyLocation = currentNode.emptyLocation + moveSet[i];
+                int nextNumber;
+                if (i == 2) {
+                    nextNumber = currentNode.number * moveSet[i];
+                } else {
+                    nextNumber = currentNode.number + moveSet[i];
+                }
 
-                if (nextEmptyLocation < 0 || nextEmptyLocation > 8
-                        || (currentNode.emptyLocation % 3 == 2 & i == 1) || (currentNode.emptyLocation % 3 == 0 & i == 3)) {
+                if (nextNumber < 0 || nextNumber > 100000 || check[nextNumber]) {
                     continue;
                 }
 
-                Node nextNode =
-                        new Node(currentNode.emptyLocation, nextEmptyLocation, currentNode.numberArr, currentNode.count + 1);
-
-                int nextPuzzleSet = intArrToInt(nextNode.numberArr);
-                if (check.containsKey(nextPuzzleSet)) {
-                    continue;
+                if (i == 2) {
+                    deque.push(new Node(nextNumber, currentNode.time));
+                } else {
+                    deque.offer(new Node(nextNumber, currentNode.time + 1));
                 }
-
-                queue.offer(nextNode);
-                check.put(nextPuzzleSet, true);
             }
         }
 
@@ -87,31 +65,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Node node = new Node();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        Map<Integer, Boolean> check = new HashMap<>();
+        boolean[] check = new boolean[100001];
+        Node node = new Node(n, 0);
 
-        for (int i = 0; i < 3; i++) {
-            
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < 3; j++) {
-                int number = Integer.parseInt(st.nextToken());
+        Node result = bfs(node, k, check);
 
-                if (number == 0) {
-                    node.emptyLocation = 3 * i + j;
-                }
-
-                node.numberArr[3 * i + j] = number;
-            }
-        }
-
-        Node result = bfs(node, check, 123456780);
-
-        if (result == null) {
-            System.out.println(-1);
-            return;
-        }
-
-        System.out.println(result.count);
+        System.out.println(result.time);
     }
 }
