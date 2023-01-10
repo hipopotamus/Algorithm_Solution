5b31507f80b6598d1c16771524cea1bd0b07e927
hipopotamus <qkqntjddn@gmail.com>
2023-01-10 15:58:53 +0900

    백준 1463번 문제_1로 만들기

--- src/main/java/solution/Main.java
+++ src/main/java/solution/Main.java
@@ -3,78 +3,24 @@ package solution;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static class Node {
+    private static void dp(int dist, int[] memorization) {
 
-        int[] bucketArr = new int[3];
+        for (int i = 2; i <= dist; i++) {
+            //1을 뺐을 때
+            memorization[i] = memorization[i - 1] + 1;
 
-        public Node(int a, int b, int c) {
-            bucketArr[0] = a;
-            bucketArr[1] = b;
-            bucketArr[2] = c;
-        }
-
-        public Node() {}
-
-        public Node getNextNode(int from, int to, int amount, int total) {
-
-            Node nextNode = new Node();
-
-            int nextFrom = this.bucketArr[from] - amount;
-            int nextTo = this.bucketArr[to] + amount;
-
-            for (int i = 0; i < 3; i++) {
-                if (i == from) {
-                    nextNode.bucketArr[i] = nextFrom;
-                } else if (i == to) {
-                    nextNode.bucketArr[i] = nextTo;
-                } else {
-                    nextNode.bucketArr[i] = total - nextFrom - nextTo;
-                }
+            //2로 나뉘어 질 때
+            if (i % 2 == 0) {
+                memorization[i] = Math.min(memorization[i], memorization[i / 2] + 1);
             }
 
-            return nextNode;
-        }
-    }
-
-    private static void bfs(Node firstNode, Node[][] nodeArr, int[] maxBucket, int total) {
-
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(firstNode);
-        int[] firstBucketArr = firstNode.bucketArr;
-        nodeArr[firstBucketArr[0]][firstBucketArr[2]] = firstNode;
-
-        while (!queue.isEmpty()) {
-
-            Node currentNode = queue.poll();
-            int[] currentBucketArr = currentNode.bucketArr;
-
-            for (int i = 0; i < 3; i++) {
-                for (int j = 1; j <= 2; j++) {
-                    //물을 붓는 경우를 나머지를 사용해서 표현
-                    int from = i;
-                    int to = (i + j) % 3;
-
-                    if (currentBucketArr[from] == 0 || currentBucketArr[to] == maxBucket[to]) {
-                        continue;
-                    }
-
-                    int amount = Math.min(maxBucket[to] - currentBucketArr[to], currentBucketArr[from]);
-
-                    Node nextNode = currentNode.getNextNode(from, to, amount, total);
-                    int[] nextBucketArr = nextNode.bucketArr;
-
-                    if (nodeArr[nextBucketArr[0]][nextBucketArr[2]] != null) {
-                        continue;
-                    }
-
-                    queue.offer(nextNode);
-                    nodeArr[nextBucketArr[0]][nextBucketArr[2]] = nextNode;
-                }
+            //3으로 나뉘어 질 때
+            if (i % 3 == 0) {
+                memorization[i] = Math.min(memorization[i], memorization[i / 3] + 1);
             }
         }
     }
@@ -82,28 +28,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int dist = Integer.parseInt(st.nextToken());
 
-        int[] maxBucket = new int[3];
-        maxBucket[0] = Integer.parseInt(st.nextToken());
-        maxBucket[1] = Integer.parseInt(st.nextToken());
-        maxBucket[2] = Integer.parseInt(st.nextToken());
+        //memorization[i] = i에서 1로 가는 최소 횟수
+        int[] memorization = new int[dist + 1];
 
-        //A를 행 C를 열으로 표현한다.
-        //B는 A와 C로 유추할 수 있다.
-        Node[][] nodeArr = new Node[maxBucket[0] + 1][maxBucket[2] + 1];
+        dp(dist, memorization);
 
-        Node firstNode = new Node(0, 0, maxBucket[2]);
-
-        bfs(firstNode, nodeArr, maxBucket, maxBucket[2]);
-
-        List<Integer> result = Arrays.stream(nodeArr[0])
-                .filter(Objects::nonNull)
-                .map(node -> node.bucketArr[2]).sorted(Comparator.naturalOrder())
-                .collect(Collectors.toList());
-
-        for (Integer integer : result) {
-            System.out.print(integer + " ");
-        }
+        System.out.println(memorization[dist]);
     }
 }
