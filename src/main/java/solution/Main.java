3c39f5fafd0baff795f6c816f95c9d07e2fe84f9
hipopotamus <qkqntjddn@gmail.com>
2023-01-12 17:14:31 +0900

    백준 11052번 문제_카드 구매하기

--- src/main/java/solution/Main.java
+++ src/main/java/solution/Main.java
@@ -11,25 +11,27 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int tryCount = Integer.parseInt(st.nextToken());
-        int[] memorization = new int[11];
-        memorization[0] = 1;
-        memorization[1] = 1;
-        memorization[2] = 2;
+        int dist = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < tryCount; i++) {
-            st = new StringTokenizer(br.readLine());
-            int dist = Integer.parseInt(st.nextToken());
+        int[] price = new int[dist + 1];
+        int[] memorization = new int[dist + 1];
 
-            for (int j = 3; j <= dist; j++) {
-                if (memorization[j] != 0) {
-                    continue;
-                }
-                //점화식
-                memorization[j] = memorization[j - 1] + memorization[j - 2] + memorization[j - 3];
-            }
-
-            System.out.println(memorization[dist]);
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i <= dist; i++) {
+            price[i] = Integer.parseInt(st.nextToken());
         }
+
+        //memorization[n] = 카드를 n개살 때 최대 비용
+        //memorization[n] = max(memorization[n-a] + price[a]) (0 < a <= n)
+        //카드를 n개살 때 최대 비용 = 카드를 n -a개 살때 최대 비용 + a개 들어있는 카드팩을 산 비용의 최대값
+        for (int i = 1; i <= dist; i++) {
+            int max = 0;
+            for (int j = 1; j <= i; j++) {
+                max = Math.max(max, memorization[i - j] + price[j]);
+            }
+            memorization[i] = max;
+        }
+
+        System.out.println(memorization[dist]);
     }
 }
