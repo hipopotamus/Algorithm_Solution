51519db7ac53d65e55b40daac4513d015cb90659
hipopotamus <qkqntjddn@gmail.com>
2024-01-22 15:19:00 +0900

    백준 2606번 문제_바이러스

---
+++ src/main/java/baekjoon/bfs/바이러스_2606.java
@@ -0,0 +1,63 @@
+package baekjoon.bfs;
+
+import java.io.*;
+import java.util.*;
+
+public class 바이러스_2606 {
+
+    public static class Node {
+        boolean check = false;
+        List<Node> edge = new ArrayList<>();
+
+        public Node() {}
+    }
+
+    private static int getInfectionNumber(Node firstNode) {
+
+        int infectionCount = 0;
+        Queue<Node> queue = new LinkedList<>();
+        firstNode.check = true;
+        queue.offer(firstNode);
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            for (Node edgeNode : node.edge) {
+                if (edgeNode.check) {
+                    continue;
+                }
+                edgeNode.check = true;
+                infectionCount++;
+                queue.offer(edgeNode);
+            }
+        }
+
+        return infectionCount;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int nodeSize = Integer.parseInt(br.readLine());
+
+        Node[] nodeArr = new Node[nodeSize + 1];
+        for (int i = 1; i <= nodeSize; i++) {
+            nodeArr[i] = new Node();
+        }
+
+        int edgeSize = Integer.parseInt(br.readLine());
+        for (int i = 0; i < edgeSize; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            nodeArr[from].edge.add(nodeArr[to]);
+            nodeArr[to].edge.add(nodeArr[from]);
+        }
+
+        int infectionNumber = getInfectionNumber(nodeArr[1]);
+
+        bw.write(String.valueOf(infectionNumber));
+        bw.flush();
+    }
+}
